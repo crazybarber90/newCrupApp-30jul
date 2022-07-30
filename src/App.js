04d7f981122ab8e7bc7e444de7bef95c9c9369a9
crazybarber90 <pepy90aa@gmail.com>
2022-07-30 12:44:40 +0200

    implemented bootstrap navbar converted from html to jsx and implemented react-router-dom v5.2.0 with all components in Switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Home from "./component/pages/Home";
 import About from "./component/pages/About";
+import Contact from "./component/pages/Contact";
+import Navbar from "./component/layout/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home></Home>
-      <About></About>
-    </>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/contact" component={Contact}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/layout/Navbar.js
@@ -0,0 +1,45 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary container">
+      {/* <div className="container"> */}
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          React Crud
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* </div> */}
+    </nav>
+  );
+};
+
+export default Navbar;
